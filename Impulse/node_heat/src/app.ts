5f2d6a39863427a8052a5cfcfe44624042574a38
euclidesdry <euclidesdry@gmail.com>
2021-10-20 23:17:50 +0100

    Creating Github Login Route and Callback Route :rooster:

--- Impulse/node_heat/src/app.ts
+++ Impulse/node_heat/src/app.ts
@@ -1,6 +1,17 @@
-import express from "express";
+import "dotenv/config";
+import express, {Request, Response} from "express";
 
 const app = express();
+
+app.get("/github", (request: Request, response: Response) => {
+  response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+});
+
+app.get("/sigin/callback", (request: Request, response: Response) => {
+  const { code } = request.query;
+
+  response.json(code);
+});
 
 
 app.listen(4224, () => console.log(` 🚀 Server is running on PORT: 4224!`));
