b015b76edfcb94f317f9a21453ae2027f5394308
euclidesdry <euclidesdry@gmail.com>
2021-10-22 07:13:20 +0100

    Showing  User Profile

--- Impulse/node_heat/src/routes.ts
+++ Impulse/node_heat/src/routes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
 import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
 
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
@@ -16,9 +17,7 @@ router.post(
   new CreateMessageController().handle
 );
 
-router.get(
-  "/messages/last3",
-  new GetLast3MessagesController().handle
-);
+router.get("/messages/last3", new GetLast3MessagesController().handle);
+router.get( "/profile", ensureAuthenticated, new ProfileUserController().handle);
 
 export { router };
