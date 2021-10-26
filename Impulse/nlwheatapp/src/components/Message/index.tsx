db617f26bc7e46d5c7b44bf244f442d22b9a200d
euclidesdry <euclidesdry@gmail.com>
2021-10-26 05:16:12 +0100

    Adding Message Moti Animation :runner:

--- Impulse/nlwheatapp/src/components/Message/index.tsx
+++ Impulse/nlwheatapp/src/components/Message/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { MotiView } from 'moti';
 
 import { styles } from './styles';
 
@@ -23,7 +24,12 @@ export function Message({ data }: Props) {
   const { id, text, user } = data;
 
   return (
-    <View style={styles.container}>
+    <MotiView
+      from={{ opacity: 0, translateY: -50 }}
+      animate={{ opacity: 1, translateY: 0 }}
+      transition={{ type: 'timing', duration: 700 }}
+      style={styles.container}
+    >
       <Text style={styles.message}>
         {text}
       </Text>
@@ -38,6 +44,6 @@ export function Message({ data }: Props) {
           {user.name}
         </Text>
       </View>
-    </View>
+    </MotiView>
   );
 }
